faecc56fbf591110545c045d34ba209e7235a084
Hiba-Mohamed <127333097+Hiba-Mohamed@users.noreply.github.com>
2023-12-01 19:08:25 -0330

    nav new styling

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -4,9 +4,9 @@ import { Link } from "react-router-dom";
 export function Heading() {
   const [open, setOpen] = useState(false);
   return (
-    <nav className="border-slate-50 bg-white font-nunito pb-2">
-      <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto px-4 max-w-7xl">
-        <Link to="/home" className="flex items-center">
+    <nav className="border-slate-50 bg-white font-nunito">
+      <div className="max-w-2xl flex flex-wrap items-center justify-between mx-auto px-4 max-w-7xl">
+        <Link to="/introPage" className="flex items-center">
           <img
             src="images/NAS-logo.png"
             className="sm:h-10 mr-3 h-6"
@@ -15,33 +15,34 @@ export function Heading() {
         </Link>
 
         <div id="navbar-hamburger">
-          <ul className="hidden text-sm md:flex flex  gap-6 flex-row items-center font-medium mt-2 rounded-lg">
+          <ul className="hidden text-sm md:flex flex gap-6 flex-row items-center font-medium rounded-lg">
             <li>
               <Link
-                to="/login"
-                className="block px-4 py-2 text-gray-900 rounded-3xl hover:text-white hover:bg-slate-500 border-solid border-2 border-slate-500"
+                to="/introPage"
+                className="block font-bold text-gray-900 hover:text-sky-700 py-4 hover:pb-3 px-4 hover:bg-sky-100  hover: hover:border-b-4 hover:border-sky-600"
                 onClick={() => setOpen(!open)}
               >
-                Login
-              </Link>
-            </li>
-            <li>
-              <Link
-                to="/hospitalView"
-                className="block px-4 py-2 text-gray-900 rounded-3xl hover:text-white hover:bg-slate-500 border-solid border-2 border-slate-500"
-                onClick={() => setOpen(!open)}
-              >
-                Hospital Units{" "}
+                Home
               </Link>
             </li>
 
             <li>
               <Link
                 to="/about"
-                className="block px-4 py-2 text-gray-900 rounded-3xl hover:text-white hover:bg-slate-500 border-solid border-2 border-slate-500"
+                className="block font-bold text-gray-900 hover:text-sky-700 py-4 hover:pb-3 px-4 hover:bg-sky-100  hover: hover:border-b-4 hover:border-sky-600"
                 onClick={() => setOpen(!open)}
               >
                 About NAS
+              </Link>
+            </li>
+
+            <li>
+              <Link
+                to="/hospitalView"
+                className="block font-bold text-gray-900 hover:text-sky-700 py-4 hover:pb-3 px-4 hover:bg-sky-100  hover: hover:border-b-4 hover:border-sky-600"
+                onClick={() => setOpen(!open)}
+              >
+                Hospital Units{" "}
               </Link>
             </li>
           </ul>
@@ -58,7 +59,7 @@ export function Heading() {
           onClick={() => setOpen(!open)}
           data-collapse-toggle="navbar-hamburger"
           type="button"
-          className="md:hidden inline-flex items-center justify-center p-2 w-10 h-10 ml-3 text-sm text-gray-500 rounded-lg hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200"
+          className="md:hidden inline-flex items-center justify-center p-2 w-10 h-10 ml-3 text-sm text-gray-500 rounded-lg hover:bg-sky-100 focus:outline-none focus:ring-2 focus:ring-gray-200"
           aria-controls="navbar-hamburger"
           aria-expanded="false"
         >
@@ -83,11 +84,11 @@ export function Heading() {
           className={`${open ? "block" : "hidden"} w-full`}
           id="navbar-hamburger"
         >
-          <ul className="md:hidden flex flex-col items-center font-medium mt-4 rounded-lg">
+          <ul className="md:hidden flex flex-col items-center font-medium my-4 rounded-lg">
             <li>
               <Link
-                to="/home"
-                className="block py-2 pl-3 pr-4 text-gray-900 rounded hover:text-slate-500"
+                to="/introPage"
+                className="block font-bold py-2 pl-3 pr-4 text-gray-900 rounded hover:text-sky-600"
                 aria-current="page"
                 onClick={() => setOpen(!open)}
               >
@@ -97,7 +98,7 @@ export function Heading() {
             <li>
               <Link
                 to="/account"
-                className="block py-2 pl-3 pr-4 text-gray-900 rounded hover:text-slate-500"
+                className="block font-bold py-2 pl-3 pr-4 text-gray-900 rounded hover:text-sky-600"
                 onClick={() => setOpen(!open)}
               >
                 Create an Account
@@ -106,7 +107,7 @@ export function Heading() {
             <li>
               <Link
                 to="/login"
-                className="block py-2 pl-3 pr-4 text-gray-900 rounded hover:text-slate-500"
+                className="block font-bold py-2 pl-3 pr-4 text-gray-900 rounded hover:text-sky-600"
                 onClick={() => setOpen(!open)}
               >
                 Login
@@ -115,7 +116,7 @@ export function Heading() {
             <li>
               <Link
                 to="/hospitalView"
-                className="block py-2 pl-3 pr-4 text-gray-900 rounded hover:text-slate-500"
+                className="block font-bold py-2 pl-3 pr-4 text-gray-900 rounded hover:text-sky-600"
                 onClick={() => setOpen(!open)}
               >
                 Hospital Units{" "}
@@ -125,7 +126,7 @@ export function Heading() {
             <li>
               <Link
                 to="/about"
-                className="block py-2 pl-3 pr-4 text-gray-900 rounded hover:text-slate-500"
+                className="block font-bold py-2 pl-3 pr-4 text-gray-900 rounded hover:text-sky-600"
                 onClick={() => setOpen(!open)}
               >
                 About NAS
