e2cf7a36454df65e95a5a2fda682716c853623dc
Hiba-Mohamed <127333097+Hiba-Mohamed@users.noreply.github.com>
2023-12-02 17:54:15 -0330

    Add unit page styling

--- src/pages/addingNewUnit.tsx
+++ src/pages/addingNewUnit.tsx
@@ -115,20 +115,23 @@ export function NewUnit() {
     }
   };
   return (
-    <div className="font-OpenSans min-h-screen">
+    <div className="flex flex-col items-center font-OpenSans lg:pt-24 bg-sky-50 min-h-screen">
       <div className="flex flex-col items-center justify-center py-6 sm:py-12">
         {" "}
-        <h1 className="p-6 sm:p-6 text-3xl sm:text-5xl text-center font-bold">
-          {hospitalData.hospitalName}
+        <h1 className="text-center font-bold text-2xl  sm:text-4xl p-12">
+          Create a Unit Profile for {hospitalData.hospitalName}
         </h1>
+        <h2 className="p-2 pb-8 text-center sm:max-w-xl max-w-sm sm:text-xl">
+          Enter your unit's name below to create a Unit Profile
+        </h2>
         <div>
-          {" "}
+          {/* {" "}
           <Link
             to="/getStarted"
             className="mx-auto hover:bg-cyan-500 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline bg-cyan-600"
           >
             Edit Hospital's name
-          </Link>
+          </Link> */}
         </div>
       </div>
       <div className="flex flex-col items-center justify-center">
@@ -136,9 +139,9 @@ export function NewUnit() {
         <div className="flex flex-col items-center justify-center">
           <form
             onSubmit={handleSubmit(onSubmit)}
-            className="p-12 justify-center items-center sm:gap-2 flex flex-col sm:flex-row bg-white m-4 shadow-lg rounded-xl sm:max-w-4xl"
+            className="p-12 sm:p-6 justify-center sm:gap-2 flex flex-col bg-white m-4 shadow-lg rounded-xl sm:max-w-4xl"
           >
-            <label className="font-bold text-xl text-center pb-4">
+            <label className="font-bold text-xl sm:text-lg text-start">
               New Unit's Name:
             </label>
             <div className="flex flex-col items-center">
@@ -146,7 +149,7 @@ export function NewUnit() {
               <input
                 {...register("unitName", { required: true, maxLength: 50 })}
                 type="text"
-                className="w-full px-2 appearance-none py-2 rounded-md border border-blue-300 focus:outline-none focus:ring-2 focus:ring-blue-500 sm:h-12 sm:w-80"
+                className="w-full px-2 appearance-none sm:py-0 py-2 rounded-md border border-blue-300 focus:outline-none focus:ring-2 focus:ring-blue-500 sm:h-10 sm:w-80"
               ></input>
               {errors?.unitName?.type === "required" && (
                 <p className="text-peach text-sm">This field is required</p>
@@ -157,21 +160,30 @@ export function NewUnit() {
                 </p>
               )}
             </div>
-
-            <button
-              type="submit"
-              className="mx-auto hover:bg-yellow-500 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline bg-orange-500 sm:ml-6 sm:mt-0 mt-6"
-            >
-              Submit
-            </button>
+            <div className="flex flex-row gap-6 items-center sm:py-4">
+              {" "}
+              <Link
+                to="/getStarted"
+                className="mx-auto hover:bg-blue  hover:text-white text-blue font-bold sm:pr-10 sm: sm:py-1 sm:text-sm py-2 px-4 sm:pr-4 rounded focus:outline-none focus:shadow-outline bg-white border-solid border-2 border-blue  sm:mt-0 mt-6"
+              >
+                <span className="pr-2">&#129144;</span>
+                Back
+              </Link>
+              <button
+                type="submit"
+                className="mx-auto hover:bg-lblue hover:text-blue text-white font-bold py-2 px-4  border-solid border-2 border-blue hover:border-lblue sm:px-10 sm:py-1 bo sm:text-sm rounded focus:outline-none focus:shadow-outline bg-blue sm:mt-0 mt-6"
+              >
+                Submit
+              </button>
+            </div>
           </form>{" "}
-          <div className="bg-white sm:px-8 max-w-sm  sm:max-w-xl text-xsm p-4 sm:text-md text-sm text-center mx-4 my-2">
-            {duplicateError ? (
+          {duplicateError ? (
+            <div className="bg-white sm:px-8 max-w-sm  sm:max-w-xl text-xsm p-4 sm:text-md text-sm text-center mx-4 my-2">
               <p className="text-peach ">{duplicateError}</p>
-            ) : (
-              ""
-            )}
-          </div>
+            </div>
+          ) : (
+            ""
+          )}
         </div>
       </div>
     </div>
