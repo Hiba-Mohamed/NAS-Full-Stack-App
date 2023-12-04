8a2ac953a47cfbe20d9dc3d73a104a2e46cb9a4e
Hiba-Mohamed <127333097+Hiba-Mohamed@users.noreply.github.com>
2023-12-03 20:39:43 -0330

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ export default defineConfig({
   plugins: [react()],
   base: '/NAS-Full-Stack-App/',
   optimizeDeps: {
-    include: ['*.ts', '*.d.ts', '*.tsx', '*.js', '*.jsx', 'main.tsx'],
+    include: ['*.ts', '*.d.ts', '*.tsx', '*.js', '*.jsx' ],
   },
 })
 
