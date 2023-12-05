557abc143df68a8f2417113050092b7e411462f5
Hiba-Mohamed <127333097+Hiba-Mohamed@users.noreply.github.com>
2023-12-05 20:22:09 -0330

    deploy

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/NAS-Full-Stack-App/',
+  base: 'https://hiba-mohamed.github.io/',
   optimizeDeps: {
     include: ['*.ts', '*.d.ts', '*.tsx', '*.js', '*.jsx' ],
   },
